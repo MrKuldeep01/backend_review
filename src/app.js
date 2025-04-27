35c2c9091ded87c7c725296e4ae72e2cf30d48ad
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2025-04-27 22:17:11 +0530

    middelware setupgit add .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import express from "express"
+import cors from 'cors'
+import cookieParser from "cookie-parser"
 const app = express();
-
+app.use(express.json())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true
+}))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(cookieParser())
 
 app.on("error",(error)=>{
     console.error("Error occured in Application : ",error);
